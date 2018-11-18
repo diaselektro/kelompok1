7014e6c1dd3a15917fb928bf4b39dfc44b008132
kelompok1 <Administrator@KP11-CND5431V7R.pertamina.com>
2018-11-18 22:33:47 +0700

    update database

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    remoteUrl : 'mongodb://mdbkelompok1:fYZdw6jJ6MeMLPZW0Y22BgZOdFYPqWC5vhI9Ocg37m87yLxguOi0G8oDZYvtkh4GzXsXPtgVYfOrHirM76KHeA==@mdbkelompok1.documents.azure.com:10255/?ssl=true&replicaSet=globaldb',
+    localUrl: 'mongodb://localhost:27017'
 };
