4dbd98dff518b7fa664caff26994970525487041
diaselektro <45145108+diaselektro@users.noreply.github.com>
2018-12-04 14:28:14 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://mdbkelompok1:fYZdw6jJ6MeMLPZW0Y22BgZOdFYPqWC5vhI9Ocg37m87yLxguOi0G8oDZYvtkh4GzXsXPtgVYfOrHirM76KHeA==@mdbkelompok1.documents.azure.com:10255/?ssl=true&replicaSet=globaldb',
+    remoteUrl : 'mongodb://kelompok1:gQbrOd7kRgmtgsWJmEu7YIGp8C4mJYPCFzkuKsyet83oRrgxMxgglnX0IivaSI9TtiGpNw77pFNF1o30K67OIQ==@kelompok1.documents.azure.com:10255/?ssl=true&replicaSet=globaldb',
     localUrl: 'mongodb://localhost:27017'
 };
